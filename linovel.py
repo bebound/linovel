91cdfe54bffdfe91699f37c9678c6e7ffb45239b
tonyrft <satouakatsuki@gmail.com>
2015-11-05 19:49:39 -0800

    add covert format feature
    Add ability to covert book format by calling calibre command tools.
    This feature require installing Calibre(A ebook managing application)
    and only works on Mac OS X.

--- linovel.py
+++ linovel.py
@@ -3,8 +3,8 @@
 
 Usage:
     linovel.py
-    linovel.py [-s] [-o | --output=<output_dir>] [-c | --cover=<cover_path>] <url>...
-    linovel.py <url>... [-s] [-o | --output=<output_dir>] [-c | --cover=<cover_path>]
+    linovel.py [-s] [-o | --output=<output_dir>] [-c | --cover=<cover_path>] [--format=<out_format>] <url>...
+    linovel.py <url>... [-s] [-o | --output=<output_dir>] [-c | --cover=<cover_path>] [--format=<out_format>]
     linovel.py -h | --help
     linovel.py -v | --version
 
@@ -15,6 +15,7 @@ Options:
     -s                                         Single thread
     -o=<output_dir> --output=<output_dir>      Output folder
     -c=<cover_path> --cover=<cover_path>       Cover path
+    --format=<out_format>                      Output format
     -h --help                                  Show this screen
     -v --version                               Show version
 
@@ -63,7 +64,7 @@ def check_url(url):
         return False
 
 
-def grab_volume(url, output_dir, cover_path):
+def grab_volume(url, output_dir, cover_path, out_format):
     """
     grab volume
     
@@ -71,12 +72,13 @@ def grab_volume(url, output_dir, cover_path):
         url: A string represent the url which was input by user
         output_dir: A string represent the path of the output EPUB file
         cover_file: A string represent the path of the EPUB cover
+        out_format: A string represent the output format
     """
     try:
         print('Getting:' + url)
         novel = Novel(url=url, single_thread=_SINGLE_THREAD)
         novel.get_novel_information()
-        epub = Epub(output_dir=output_dir, cover_path=cover_path, **novel.novel_information())
+        epub = Epub(output_dir=output_dir, cover_path=cover_path, out_format=out_format, **novel.novel_information())
         epub.generate_epub()
 
     except Exception as e:
@@ -99,7 +101,7 @@ def parse_page(url):
     return BeautifulSoup(r.text, 'lxml')
 
 
-def grab_booklist(url, output_dir, cover_path):
+def grab_booklist(url, output_dir, cover_path, out_format):
     """
     grab each volume in the booklist
 
@@ -112,10 +114,10 @@ def grab_booklist(url, output_dir, cover_path):
     volume_links = soup.select('li.linovel-book-item h3 a')
     for volume in volume_links:
         volume_link = 'http://www.linovel.com' + volume['href']
-        grab_volume(volume_link, output_dir, cover_path)
+        grab_volume(volume_link, output_dir, cover_path, out_format)
 
 
-def start(urls, output_dir=None, cover_path=None):
+def start(urls, output_dir=None, cover_path=None, out_format=None):
     """
     start the job using url
 
@@ -127,9 +129,9 @@ def start(urls, output_dir=None, cover_path=None):
     for url in urls:
         check_result = check_url(url)
         if check_result == 'book':
-            grab_volume(url, output_dir, cover_path)
+            grab_volume(url, output_dir, cover_path, out_format)
         elif check_result == 'vollist':
-            grab_booklist(url, output_dir, cover_path)
+            grab_booklist(url, output_dir, cover_path, out_format)
         else:
             print('请输入正确的网址，例如：\nhttp://www.linovel.com/n/vollist/492.html'
                   '\nhttp://www.linovel.com/n/book/1578.html')
@@ -142,14 +144,16 @@ def main():
         _SINGLE_THREAD = arguments['-s']
         output_dir = None if not arguments['--output'] else arguments['--output'][0]
         cover_path = None if not arguments['--cover'] else arguments['--cover'][0]
+        out_format = 'epub' if not arguments['--format'] else arguments['--format']
     else:
         urls = input('Please input urls（separate with space）:').split()
         if is_single_thread():
             _SINGLE_THREAD = True
         output_dir = None
         cover_path = None
+        out_format = None
     if urls:
-        start(urls, output_dir, cover_path)
+        start(urls, output_dir, cover_path, out_format)
     else:
         raise RuntimeError('No url')
 
