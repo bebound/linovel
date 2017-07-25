c05b40e13ea0fabae07140fbbe82cfa25956e6cb
KK <bebound@gmail.com>
2017-07-26 01:16:10 +0800

    Update test

--- tests/test_360dxs.py
+++ tests/test_360dxs.py
@@ -7,16 +7,16 @@ from novel_360dxs import Dxs
 
 
 class TestDxs:
-    novel = Dxs('http://qitawenku.360dxs.com/book_3037.html')
+    novel = Dxs('http://dianjiwenku.360dxs.com/book_1672.html')
 
     def test_all(self):
         assert self.novel.extract_novel_information() is None
 
     def test_bookname(self):
-        assert self.novel.novel_information[0]['book_name'] == 'No game No life游戏人生'
+        assert self.novel.novel_information[0]['book_name'] == '线上游戏的老婆不可能是女生？'
 
     def test_author(self):
-        assert self.novel.novel_information[0]['author'] == '榎宫佑'
+        assert self.novel.novel_information[0]['author'] == '听猫芝居'
 
     def test_chapters(self):
         assert len(self.novel.novel_information[0]['chapters']) == 7
@@ -25,7 +25,7 @@ class TestDxs:
         assert self.novel.novel_information[0]['volume_number'] == '第一卷'
 
     def test_volume_name(self):
-        assert self.novel.novel_information[0]['volume_name'] == '听说游戏玩家兄妹要征服幻想世界'
+        assert self.novel.novel_information[0]['volume_name'] == ''
 
     def test_filename(self):
-        assert self.novel.novel_information[0]['filename'] == 'No game No life游戏人生 第一卷 听说游戏玩家兄妹要征服幻想世界'
+        assert self.novel.novel_information[0]['filename'] == '线上游戏的老婆不可能是女生？ 第一卷'
